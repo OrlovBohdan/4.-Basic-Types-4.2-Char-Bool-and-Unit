07004f1475cadb26da6c5674f998c119af104663
OrlovBohdan <b.orlov@e-u.edu.ua>
2024-09-29 13:02:34 +0300

    1

--- src/task1.rs
+++ src/task1.rs
@@ -1,7 +1,24 @@
+use std::mem::size_of_val;
 #[test]
+/*// Make it work
+use std::mem::size_of_val;
+fn main() {
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1),1);
+
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2),3);
+
+    println!("Success!");
+} */
+// Make it work
 
 fn main() {
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1),4);
 
-    let x =1;
-    println!("Hello, world!");
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2),4);
+
+    println!("Success!");
 }
